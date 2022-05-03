2855768e4748dad9d14f3826d85ec2356589acc9
gustavo-machado-afterverse <gustavo-machado@afterverse.com>
2022-05-03 11:50:47 -0300

    Added some dictionary extensions

--- Extensions/DictionaryExtension.cs
+++ Extensions/DictionaryExtension.cs
@@ -17,5 +17,53 @@ namespace LegendaryTools
                 dictionary.Add(key, value);
             }
         }
+        
+        public static V[] ValuesToArray<K,V>(this IDictionary<K,V> dictionary)
+        {
+            V[] result = new V[dictionary.Count];
+            int i = 0;
+            foreach (KeyValuePair<K, V> pair in dictionary)
+            {
+                result[i] = pair.Value;
+                i++;
+            }
+            return result;
+        }
+        
+        public static K[] KeysToArray<K,V>(this IDictionary<K,V> dictionary)
+        {
+            K[] result = new K[dictionary.Count];
+            int i = 0;
+            foreach (KeyValuePair<K, V> pair in dictionary)
+            {
+                result[i] = pair.Key;
+                i++;
+            }
+            return result;
+        }
+        
+        public static List<V> ValuesToList<K,V>(this IDictionary<K,V> dictionary)
+        {
+            List<V> result = new List<V>(dictionary.Count); 
+            int i = 0;
+            foreach (KeyValuePair<K, V> pair in dictionary)
+            {
+                result[i] = pair.Value;
+                i++;
+            }
+            return result;
+        }
+        
+        public static List<K> KeysToList<K,V>(this IDictionary<K,V> dictionary)
+        {
+            List<K> result = new List<K>(dictionary.Count);
+            int i = 0;
+            foreach (KeyValuePair<K, V> pair in dictionary)
+            {
+                result[i] = pair.Key;
+                i++;
+            }
+            return result;
+        }
     }
 }
